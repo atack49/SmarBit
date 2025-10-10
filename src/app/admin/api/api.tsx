0b621aa9029707f4dc8b66f54e78068c5767c499
Sharany25 <osoriodiego877@gmail.com>
2025-10-09 21:09:35 -0600

    Pruebas aws

--- src/app/admin/api/api.tsx
+++ src/app/admin/api/api.tsx
@@ -2,4 +2,4 @@
 //export const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:3000/MyFitGuide";
 
 //API en servidor
-export const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL || "https://myfitguideapi-production.up.railway.app/MyFitGuide/";
+export const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL || "http://18.220.28.9:3000/MyFitGuide";
