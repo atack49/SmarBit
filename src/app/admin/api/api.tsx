73599a8f664f9e5a6eb6ca7edbfa51fe53c939a3
Sharany25 <osoriodiego877@gmail.com>
2025-10-20 10:43:26 -0600

    Detalles finales Post EC2

--- src/app/admin/api/api.tsx
+++ src/app/admin/api/api.tsx
@@ -5,4 +5,4 @@
 //export const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL || "http://18.220.28.9:3000/MyFitGuide";
 
 //API en EC2 usando HTTPS con dominio
-export const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL || "https://api.myfitguide.com.mx/MyFitGuide";
+export const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL || "https://myfitguide.duckdns.org/MyFitGuide";
